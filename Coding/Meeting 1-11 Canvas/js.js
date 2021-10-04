e4bc8e1d6bb12b061f997cb72abbf999f4233d55
Terryhawks <88441063+Terryhawks@users.noreply.github.com>
2021-10-04 21:09:09 +0700

    Update 8 - Chaos

--- Coding/Meeting 1-11 Canvas/js.js
+++ Coding/Meeting 1-11 Canvas/js.js
@@ -1,9 +1,11 @@
-var canvas = document.getElementById("c1");
+    var canvas = document.getElementById("c1");
 var ctx = c1.getContext("2d");
 
 var grd = ctx.createLinearGradient(0, 0, 1580, 0);
 grd.addColorStop(0, "midnightblue");
-grd.addColorStop(1, "deepSkyBlue");
+grd.addColorStop(0.5, "deepSkyBlue");
+grd.addColorStop(1, "dodgerblue");
+
 ctx.fillStyle = grd;
 ctx.fillRect(0, 0, 1580, 760);
 
@@ -19,7 +21,8 @@ ctx.textAlign = "center"
 ctx.font = "30px Cursive"
 ctx.fillText(text, 793, 375)
 
-var canvas = new fabric.Canvas('c1');
-canvas.isDrawingMode = true;
-canvas.freeDrawingBrush.width = 5;
-console.log(canvas);
+text = "Spiderbite"
+ctx.fillStyle = "darkslateblue"
+ctx.textAlign = "center"
+ctx.font = "15px Noto Sans"
+ctx.fillText(text, 250, 400)
