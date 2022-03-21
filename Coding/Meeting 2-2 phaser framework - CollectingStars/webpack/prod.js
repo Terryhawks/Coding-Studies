b4f04852956f6790c2b5538cb7e9e95b1aa7cc1f
Terryhawks <88441063+Terryhawks@users.noreply.github.com>
2022-03-21 19:29:23 +0700

    Yet Another Test

---
+++ Coding/Meeting 2-2 phaser framework - CollectingStars/webpack/prod.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-undef */
+const merge = require("webpack-merge");
+const path = require("path");
+const base = require("./base");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(base, {
+  mode: "production",
+  output: {
+    filename: "bundle.min.js",
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devtool: false,
+  performance: {
+    maxEntrypointSize: 900000,
+    maxAssetSize: 900000
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            comments: false
+          }
+        }
+      })
+    ]
+  }
+});
